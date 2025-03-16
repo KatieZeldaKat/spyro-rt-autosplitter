f1b184bf4551c7f7e430689172f11299b283e20f
Katherine Norton <katherine@queerkat.games>
2025-03-15 22:48:14 -0500

    Prevent null reads into the current map

--- src/lib.rs
+++ src/lib.rs
@@ -111,34 +111,35 @@ async fn continue_run(process: &Process, address: &Address, settings: &Settings)
         }
 
         // Automatically split on map change
-        let map = map_watcher.update_infallible(get_map(&process, &address));
-        if map.changed() && is_valid_map_transition(&map) {
-            match settings.get_map_split_setting(&map.old) {
-                Split::FirstTime => if has_split.insert(map.old.clone()) {
-                    timer::split();
-                },
-                Split::EveryTime => {
-                    timer::split();
-                },
-                Split::Never => {},
-            };
-        }
+        if let Some(map) = map_watcher.update(get_map(&process, &address)) {
+            if map.changed() && is_valid_map_transition(&map) {
+                match settings.get_map_split_setting(&map.old) {
+                    Split::FirstTime => if has_split.insert(map.old.clone()) {
+                        timer::split();
+                    },
+                    Split::EveryTime => {
+                        timer::split();
+                    },
+                    Split::Never => {},
+                };
+            }
 
-        // Split on kill for boss fights
-        match &map.current as &str {
-            "/LS227_RiptosArena/Maps/" => {
-                let health = get_ripto_health(&process, &address);
-                if boss_killed(&mut boss_watcher, health) && settings.s2_ripto_kill {
-                    timer::split();
-                }
-            },
-            "/LS335_SorceressLair/Maps/" => {
-                let health = get_sorceress_lair_health(&process, &address);
-                if boss_killed(&mut boss_watcher, health) && settings.s3_sorceress_lair_kill {
-                    timer::split();
-                }
-            },
-            _ => {}
+            // Split on kill for boss fights
+            match &map.current as &str {
+                "/LS227_RiptosArena/Maps/" => {
+                    let health = get_ripto_health(&process, &address);
+                    if boss_killed(&mut boss_watcher, health) && settings.s2_ripto_kill {
+                        timer::split();
+                    }
+                },
+                "/LS335_SorceressLair/Maps/" => {
+                    let health = get_sorceress_lair_health(&process, &address);
+                    if boss_killed(&mut boss_watcher, health) && settings.s3_sorceress_lair_kill {
+                        timer::split();
+                    }
+                },
+                _ => {}
+            }
         }
 
         next_tick().await;
@@ -178,7 +179,7 @@ fn boss_killed(boss_watcher: &mut Watcher<u8>, current_health: u8) -> bool {
 
 fn get_in_control(process: &Process, address: &Address) -> bool {
     let path = &[0x03415F30, 0xF8, 0x478];
-    let in_control_raw = safely_read_memory(&process, &address, path, u8::default());
+    let in_control_raw = get_raw_or_default(&process, &address, path, u8::default());
 
     // Set to 0 when immobilized, 1 once Spyro can move
     let in_control = in_control_raw > 0;
@@ -190,7 +191,7 @@ fn get_in_control(process: &Process, address: &Address) -> bool {
 
 fn get_is_loading(process: &Process, address: &Address) -> bool {
     let path = &[0x03415F30, 0xF8, 0x4A8, 0xE19];
-    let is_loading_raw = safely_read_memory(&process, &address, path, u8::default());
+    let is_loading_raw = get_raw_or_default(&process, &address, path, u8::default());
 
     // Set to 0 when loading, set to 1 otherwise
     let is_loading = is_loading_raw == 0;
@@ -202,7 +203,7 @@ fn get_is_loading(process: &Process, address: &Address) -> bool {
 
 fn get_in_menu(process: &Process, address: &Address) -> bool {
     let path = &[0x034160D0, 0x20, 0x218, 0x60];
-    let in_menu_raw = safely_read_memory(&process, &address, path, u8::default());
+    let in_menu_raw = get_raw_or_default(&process, &address, path, u8::default());
 
     // Set to 0 in game, 1 if in menu, 15 if in graphics submenu
     let in_menu = in_menu_raw > 0;
@@ -214,7 +215,7 @@ fn get_in_menu(process: &Process, address: &Address) -> bool {
 
 fn get_in_game(process: &Process, address: &Address) -> bool {
     let path = &[0x03415F30, 0xF0, 0x378, 0x564];
-    let in_game_raw = safely_read_memory(&process, &address, path, u8::default());
+    let in_game_raw = get_raw_or_default(&process, &address, path, u8::default());
 
     // Set to 0 in title screen and main menu, set to 1 everywhere else
     let in_game = in_game_raw > 0;
@@ -228,7 +229,7 @@ fn get_game(process: &Process, address: &Address) -> u8 {
     let path = &[0x03415F30, 0xF8, 0x290, 0x0, 0x1F8];
 
     // Set to 0 in title screen, 1-3 corresponding to Spyro 1-3
-    let game = safely_read_memory(&process, &address, path, u8::default());
+    let game = get_raw_or_default(&process, &address, path, u8::default());
 
     timer::set_variable("game", &game.to_string());
 
@@ -239,7 +240,7 @@ fn get_ripto_health(process: &Process, address: &Address) -> u8 {
     let path = &[0x03415F30, 0x110, 0x50, 0x140, 0x8, 0x1D0, 0x134];
 
     // Set to 8 at beginning of fight, decreases towards 0 as damage is taken in 3rd phase
-    let ripto_health = safely_read_memory(&process, &address, path, 8);
+    let ripto_health = get_raw_or_default(&process, &address, path, 8);
 
     timer::set_variable("ripto_health", &ripto_health.to_string());
 
@@ -250,26 +251,38 @@ fn get_sorceress_lair_health(process: &Process, address: &Address) -> u8 {
     let path = &[0x03601278, 0x40, 0x58, 0x20, 0xB0, 0x90, 0x140, 0xA28];
 
     // Set to 10 at beginning of fight, decreases towards 0 as damage is taken
-    let sorceress_lair_health = safely_read_memory(&process, &address, path, 10);
+    let sorceress_lair_health = get_raw_or_default(&process, &address, path, 10);
 
     timer::set_variable("sorceress_lair_health", &sorceress_lair_health.to_string());
 
     return sorceress_lair_health;
 }
 
-fn get_map(process: &Process, address: &Address) -> String {
+fn get_map(process: &Process, address: &Address) -> Option<String> {
     let path = &[0x03415F30, 0x138, 0xB0, 0xB0, 0x598, 0x210, 0xB8, 0x148, 0x190, 0x0];
-    let map_raw = safely_read_memory(&process, &address, path, ArrayWString::<256>::default());
+    let map_raw = get_raw::<ArrayWString::<256>>(&process, &address, path);
+
+    if map_raw.is_none() {
+        return None;
+    }
 
     // String that looks like a folder path (i.e. "/LS102_StoneHill/Maps/")
-    let map = String::from_utf16(&map_raw.as_slice()).unwrap();
+    let map = String::from_utf16(&map_raw.unwrap().as_slice()).unwrap();
 
-    timer::set_variable("map", &map,);
+    timer::set_variable("map", &map);
 
-    return map;
+    return Some(map);
 }
 
-fn safely_read_memory<T: Pod>(process: &Process, address: &Address, path: &[u64], default: T) -> T {
+fn get_raw<T: Pod>(process: &Process, address: &Address, path: &[u64]) -> Option<T> {
+    if let Ok(data) = process.read_pointer_path::<T>(*address, PointerSize::Bit64, path) {
+        return Some(data);
+    }
+
+    return None;
+}
+
+fn get_raw_or_default<T: Pod>(process: &Process, address: &Address, path: &[u64], default: T) -> T {
     if let Ok(data) = process.read_pointer_path::<T>(*address, PointerSize::Bit64, path) {
         return data;
     }
